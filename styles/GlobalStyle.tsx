aaee993dd6b162c43b16504fe92720cc68a9bb33
Kimseonghyoek <cilo1223@naver.com>
2023-01-02 03:41:14 +0900

    Global Styled

---
+++ styles/GlobalStyle.tsx
@@ -0,0 +1,14 @@
+import {createGlobalStyle} from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  body {
+    margin: 0;
+  }
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+export default GlobalStyle;
