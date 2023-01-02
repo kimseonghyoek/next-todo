14ba811d8171b44c26b3fb77ff879decf6101afd
Kimseonghyoek <cilo1223@naver.com>
2023-01-03 02:37:46 +0900

    Create Components

---
+++ components/TodoList.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+`
+
+const TodoList: React.FC = () => {
+  return (
+    <Container>
+      <h1>TodoList</h1>
+    </Container>
+  )
+}
+
+export default TodoList;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,10 @@
 import React from "react";
 import { NextPage } from "next";
 import styled from "styled-components";
-
-const Container = styled.div`
-  padding: 20px;
-`;
+import TodoList from "../components/TodoList";
 
 const index: NextPage = () => {
-  return <Container>
-    <h1>Hello Styled-Components</h1>
-    <h2>Hello Styled-Components</h2>
-    <p>Hello Styled-Components</p>
-    <ul>
-      <li>Hello Styled-Components</li>
-    </ul>
-    <a>Hello Styled-Components</a>
-    <span>Hello Styled-Components</span>
-  </Container>;
+  return <TodoList/>
 };
 
 export default index;
