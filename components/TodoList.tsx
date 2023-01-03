e13490c85e8316102f4dc2f68375c973dce6b77a
Kimseonghyoek <cilo1223@naver.com>
2023-01-04 04:38:15 +0900

    Create Todo item list

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,13 +1,35 @@
 import styled from "styled-components";
+import { TodoType } from "../types/todo";
+import palette from "../styles/palette";
+
+interface IProps {
+  todos: TodoType[];
+}
 
 const Container = styled.div`
   width: 100%;
+
+  .todo-list-header {
+    padding: 12px;
+    border-bottom: 1px solid ${palette.gray};
+
+    .todo-list-last-todo {
+      font-size: 14px;
+      span {
+        margin-left: 8px;
+      }
+    }
+  }
 `
 
-const TodoList: React.FC = () => {
+const TodoList: React.FC<IProps> = ({ todos }) => {
   return (
     <Container>
-      <h1>TodoList</h1>
+      <div className="todo-list-header">
+        <p className="todo-list-last-todo">
+        남은 todo<span>{todos.length}개</span>
+        </p>
+      </div>
     </Container>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,19 @@ import React from "react";
 import { NextPage } from "next";
 import styled from "styled-components";
 import TodoList from "../components/TodoList";
+import { TodoType } from "../types/todo";
+
+const todos: TodoType[] = [
+  { id: 1, text: "마트 가서 장보기", color: "red", checked: false },
+  { id: 2, text: "코딩하기", color: "orange", checked: true },
+  { id: 3, text: "Nest.js 연습하기", color: "yellow", checked: false },
+  { id: 4, text: "Next.js 연습하기", color: "green", checked: true },
+  { id: 5, text: "게임 개발하기", color: "blue", checked: false },
+  { id: 6, text: "분리수거 하기", color: "navy", checked: false },
+]
 
 const index: NextPage = () => {
-  return <TodoList/>
+  return <TodoList todos={todos}/>
 };
 
 export default index;
