4b41d038654f512d6007c05f5010479bea3e19f6
Kimseonghyoek <cilo1223@naver.com>
2023-01-05 19:30:29 +0900

    Update Todo item list

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -10,23 +10,97 @@ interface IProps {
 const Container = styled.div`
   width: 100%;
 
+  .todo-num {
+    margin-left: 12px;
+  }
+
   .todo-list-header {
     padding: 12px;
+    position: relative;
     border-bottom: 1px solid ${palette.gray};
 
     .todo-list-last-todo {
       font-size: 14px;
+      margin: 0 0 8px;
       span {
-        margin-left: 8px;
+        margin-left: 12px;
       }
     }
+
+    .todo-list-header-colors {
+      display: flex;
+      .todo-list-header-color-num {
+        display: flex;
+        margin-right: 8px;
+        p {
+          font-size: 14px;
+          line-height: 16px;
+          margin: 0;
+          margin-left: 6px;
+        }
+        .todo-list-header-round-color {
+          width: 16px;
+          height: 16px;
+          border-radius: 50%;
+        }
+      }
+    }
+  }
+
+  .todo-list {
+    .todo-item {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+      height: 52px;
+      border-bottom: 1px solid ${palette.gray};
+
+      .todo-left-side {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        .todo-color-block {
+          width: 12px;
+          height: 100%;
+        }
+        .checked-todo-text {
+          color: ${palette.gray};
+          text-decoration: line-through;
+        }
+        .todo-text {
+          margin-left: 12px;
+          font-size: 16px;
+        }
+      }
+    }
+  }
+
+  .bg-blue {
+    background-color: ${palette.blue};
+  }
+  .bg-green {
+    background-color: ${palette.green};
+  }
+  .bg-navy {
+    background-color: ${palette.navy};
+  }
+  .bg-orange {
+    background-color: ${palette.orange};
+  }
+  .bg-red {
+    background-color: ${palette.red};
+  }
+  .bg-yellow {
+    background-color: ${palette.yellow};
   }
 `;
 
 // 객체의 문자열 인덱스 사용을 위한 타입
 type ObjectIndexType = {
   [key: string]: number | undefined;
-}
+};
 
 const TodoList: React.FC<IProps> = ({ todos }) => {
   // const getTodoColorNums = useCallback(() => {
@@ -76,7 +150,7 @@ const TodoList: React.FC<IProps> = ({ todos }) => {
     const colors: ObjectIndexType = {};
     todos.forEach((todo) => {
       const value = colors[todo.color];
-      if(!value) {
+      if (!value) {
         colors[`${todo.color}`] = 1;
       } else {
         colors[`${todo.color}`] = value + 1;
@@ -85,7 +159,9 @@ const TodoList: React.FC<IProps> = ({ todos }) => {
     return colors;
   }, [todos]);
 
-  console.log(todoColorNum2)
+  console.log(todoColorNum2);
+
+  const todo = todoColorNum2;
 
   return (
     <Container>
@@ -93,7 +169,31 @@ const TodoList: React.FC<IProps> = ({ todos }) => {
         <p className="todo-list-last-todo">
           남은 todo<span>{todos.length}개</span>
         </p>
+        <div className="todo-list-header-colors">
+          {Object.keys(todo).map((color, index) => (
+            <div className="todo-list-header-color-num" key={index}>
+              <div className={`todo-list-header-round-color bg-${color}`}></div>
+              <p>{todo[color]}개</p>
+            </div>
+          ))}
+        </div>
       </div>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <li className="todo-item" key={todo.id}>
+            <div className="todo-left-side">
+              <div className={`todo-color-block bg-${todo.color}`} />
+              <p
+                className={`todo-text ${
+                  todo.checked ? "checked-todo-text" : ""
+                }`}
+              >
+                {todo.text}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </Container>
   );
 };
