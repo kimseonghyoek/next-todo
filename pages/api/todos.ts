7b8292e4ecfdf06df71b69e55014246eb475b4e0
Kimseonghyoek <cilo1223@naver.com>
2023-01-08 03:56:08 +0900

    GET todo items

--- pages/api/todos.ts
+++ pages/api/todos.ts
@@ -1,5 +1,33 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { TodoType } from "../../types/todo";
+import fs from 'fs';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  return res.send("Hello Next!!!");
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "GET") {
+    try {
+      const todos = await new Promise<TodoType[]>((resolve, reject) => {
+        fs.readFile("./data/todos.json", (err, data) => {
+          if(err) {
+            return reject(err.message);
+          }
+          const todoData = data.toString();
+          if (!todoData) {
+            return resolve([]);
+          }
+          const todos = JSON.parse(data.toString());
+          return resolve(todos);
+        });
+      });
+      res.statusCode = 200;
+      return res.send(todos);
+    } catch (e) {
+      console.log(e);
+      res.statusCode = 500;
+      res.send(e);
+    }
+  }
+
+  res.statusCode = 405;
+  console.log(res.statusCode);
+  return res.end();
 };
