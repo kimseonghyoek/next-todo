5fec374b20a0992409a59f972c0599e8cc520941
Kimseonghyoek <cilo1223@naver.com>
2023-01-15 19:39:48 +0900

    Update Add Todo page

--- components/AddTodo.tsx
+++ components/AddTodo.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import palette from "../styles/palette";
+import BrushIcon from "../public/static/svg/brush.svg";
+import { TodoType } from "../types/todo";
 
 const Container = styled.div`
   padding: 16px;
@@ -22,15 +25,83 @@ const Container = styled.div`
       font-size: 14px;
     }
   }
+
+  .add-todo-color-wrapper {
+    width: 100%;
+    margin-top: 16px;
+    display: flex;
+    justify-content: space-between;
+
+    .add-todo-color-list {
+      display: flex;
+      button {
+        width: 24px;
+        height: 24px;
+        margin-right: 16px;
+        border: 0;
+        outline: 0;
+        border-radius: 50%;
+        &:last-child {
+          margin: 0;
+        }
+      }
+      .add-todo-selected-color {
+        border: 2px solid black !important;
+      }
+    }
+  }
+  .bg-blue {
+    background-color: ${palette.blue}
+  }
+  .bg-green {
+    background-color: ${palette.green}
+  }
+  .bg-navy {
+    background-color: ${palette.navy}
+  }
+  .bg-orange {
+    background-color: ${palette.orange}
+  }
+  .bg-red {
+    background-color: ${palette.red}
+  }
+  .bg-yellow {
+    background-color: ${palette.yellow}
+  }
+
+  textarea {
+    width: 100%;
+    border-radius: 5px;
+    height: 300px;
+    border-color: ${palette.gray};
+    margin-top: 12px;
+    resize: none;
+    outline: none;
+    padding: 12px;
+    font-size: 16px;
+  }
 `;
 
 const AddTodo: React.FC = () => {
+
+  const [text, setText] = useState("");
+  const [selectedColor, setSelectedColor] = useState<TodoType["color"]>();
+
   return (
     <Container>
       <div className="add-todo-header">
         <h1 className="add-todo-header-title">Add Todo</h1>
         <button type="button" className="add-todo-submit-button" onClick={()=>{}}>추가하기</button>
       </div>
+      <div className="add-todo-color-wrapper">
+        <div className="add-todo-color-list">
+          {["red", "orange", "yellow", "green", "blue", "navy"].map((color, index) => (
+            <button key={index} type="button" className={`bg-${color} add-todo-color-button ${color === selectedColor ? "add-todo-selected-color" : ""}`} onClick={() => setSelectedColor(color as TodoType["color"])}/>
+          ))}
+        </div>
+        <BrushIcon/>
+      </div>
+      <textarea value={text} onChange={(e) => setText(e.currentTarget.value)} placeholder="할 일을 입력해주세요."/>
     </Container>
   )
 }
